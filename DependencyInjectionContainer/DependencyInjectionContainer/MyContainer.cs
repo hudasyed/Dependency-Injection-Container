79b749ff2d3b17c81887974f1681c8a3cb72c5ff
hudasyed <who.the.syed@gmail.com>
2018-09-29 16:23:34 -0700

    A. Register class type and Resolve the registered class as a new instance.

--- DependencyInjectionContainer/DependencyInjectionContainer/MyContainer.cs
+++ DependencyInjectionContainer/DependencyInjectionContainer/MyContainer.cs
@@ -4,5 +4,33 @@ namespace DependencyInjectionContainer
 {
     public class MyContainer
     {
+        private Type objectType;
+
+        public MyContainer() { }
+
+        public void Register<Interface, Class>()
+        {
+            Type i = typeof(Interface);
+            Type c = typeof(Class);
+
+            if (!i.IsAssignableFrom(c))
+            {
+                throw new ArgumentException($"{c.Name} does not implement {i.Name}");
+            }
+
+            objectType = c;
+        }
+
+        public object Resolve<Interface>()
+        {
+            Type i = typeof(Interface);
+
+            if (!i.IsAssignableFrom(objectType))
+            {
+                throw new ArgumentException($"{objectType.Name} does not implement {i.Name}");
+            }
+
+            return Activator.CreateInstance(objectType);
+        }
     }
 }
--- DependencyInjectionContainer/DependencyInjectionContainerTests/MyContainerTests.cs
+++ DependencyInjectionContainer/DependencyInjectionContainerTests/MyContainerTests.cs
@@ -1,3 +1,4 @@
+using DependencyInjectionContainer;
 using System;
 using Xunit;
 
@@ -5,10 +6,31 @@ namespace DependencyInjectionContainerTests
 {
     public class MyContainerTests
     {
-        [Fact]
-        public void Test1()
-        {
+        public interface IFoo { }
 
+        public class Foo : IFoo { }
+
+        [Fact]
+        public void when_registering_a_type_should_return_an_instance_of_that_type()
+        {
+            var ioc = new MyContainer();
+            ioc.Register<IFoo, Foo>();
+
+            var instance1 = ioc.Resolve<IFoo>();
+
+            Assert.True(instance1 is IFoo);
+        }
+
+        [Fact]
+        public void when_registered_a_type_resolve_should_return_a_new_instance_of_that_type()
+        {
+            var ioc = new MyContainer();
+            ioc.Register<IFoo, Foo>();
+
+            var instance1 = ioc.Resolve<IFoo>();
+            var instance2 = ioc.Resolve<IFoo>();
+
+            Assert.True(instance1 != instance2);
         }
     }
 }
