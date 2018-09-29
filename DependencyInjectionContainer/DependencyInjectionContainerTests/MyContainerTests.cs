4a432d0f1a386a2d7cb0616231564995e5e77f12
hudasyed <who.the.syed@gmail.com>
2018-09-29 15:58:49 -0700

    Scaffolding Library and Test Project.

---
+++ DependencyInjectionContainer/DependencyInjectionContainerTests/MyContainerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DependencyInjectionContainerTests
+{
+    public class MyContainerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
